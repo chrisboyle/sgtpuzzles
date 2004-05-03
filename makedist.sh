6dc05307d6741c2a8a838b9e6bf61607ba4ee6ea
Simon Tatham <anakin@pobox.com>
2004-05-03 13:03:12 +0000

    Shell script to prepare a source distribution archive.
    [originally from svn r4193]

---
+++ makedist.sh
@@ -0,0 +1,14 @@
+#!/bin/sh 
+
+perl mkfiles.pl
+
+mkdir tmp.$$
+mkdir tmp.$$/puzzles
+
+for i in *.c *.h LICENCE README Recipe mkfiles.pl Makefile.*; do
+  ln -s ../../$i tmp.$$/puzzles
+done
+
+tar -C tmp.$$ -chzvf - puzzles > ../puzzles.tar.gz
+
+rm -rf tmp.$$
