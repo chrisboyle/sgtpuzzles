6328483bb4206bed33e261e39b1a4d416adf2032
Simon Tatham <anakin@pobox.com>
2009-01-06 23:21:42 +0000

    Bring the unfinished .R files into line with the current conventions.
    [originally from svn r8395]

--- unfinished/sokoban.R
+++ unfinished/sokoban.R
@@ -4,7 +4,7 @@ sokoban  : [X] GTK COMMON sokoban sokoban-icon|no-icon
 
 sokoban  : [G] WINDOWS COMMON sokoban sokoban.res?
 
-ALL += sokoban
+ALL += sokoban[COMBINED]
 
 !begin gtk
 GAMES += sokoban
@@ -12,4 +12,8 @@ GAMES += sokoban
 
 !begin >list.c
     A(sokoban) \
+!end
+
+!begin >wingames.lst
+sokoban.exe:Sokoban
 !end
