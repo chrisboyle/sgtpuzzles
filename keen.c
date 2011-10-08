99fdfd53362d75055fc2f3f62167224d4f9504e4
Chris Boyle <chris@boyle.name>
2011-10-08 18:48:55 +0100

    Stay in cursor mode, like Solo.

--- keen.c
+++ keen.c
@@ -1359,7 +1359,13 @@ static game_ui *new_ui(game_state *state)
     game_ui *ui = snew(game_ui);
 
     ui->hx = ui->hy = 0;
-    ui->hpencil = ui->hshow = ui->hcursor = 0;
+    ui->hpencil = ui->hshow = 0;
+    ui->hcursor =
+#ifdef ANDROID
+        1;  /* and never unset */
+#else
+        0;
+#endif
 
     return ui;
 }
@@ -1551,7 +1557,10 @@ static char *interpret_move(game_state *state, game_ui *ui, game_drawstate *ds,
                 ui->hshow = 1;
                 ui->hpencil = 0;
             }
+#ifndef ANDROID
+            /* Android is always in cursor mode */
             ui->hcursor = 0;
+#endif
             return "";		       /* UI activity occurred */
         }
         if (button == RIGHT_BUTTON) {
@@ -1571,7 +1580,10 @@ static char *interpret_move(game_state *state, game_ui *ui, game_drawstate *ds,
             } else {
                 ui->hshow = 0;
             }
+#ifndef ANDROID
+            /* Android is always in cursor mode */
             ui->hcursor = 0;
+#endif
             return "";		       /* UI activity occurred */
         }
     }
