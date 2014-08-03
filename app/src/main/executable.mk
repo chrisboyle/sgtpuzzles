d4757197a5132de5a898785084512faef4239485
Chris Boyle <chris@boyle.name>
2014-08-03 22:15:00 +0100

    Migrate to Android Studio, part 1

---
+++ app/src/main/executable.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(call my-dir)
+
+# Built earlier by Gradle's generated Android.mk
+include $(CLEAR_VARS)
+LOCAL_MODULE    := libpuzzles-prebuilt
+LOCAL_SRC_FILES := $(NDK_LIBS_OUT)/../prebuilt/$(TARGET_ARCH_ABI)/libpuzzles.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := puzzlesgen$(PUZZLESGEN_SUFFIX)
+LOCAL_CFLAGS    := -DSLOW_SYSTEM -DANDROID -DSTYLUS_BASED -DNO_PRINTING -DCOMBINED -DEXECUTABLE
+LOCAL_SRC_FILES := jni/android-gen.c
+LOCAL_SHARED_LIBRARIES := libpuzzles-prebuilt
+include $(BUILD_EXECUTABLE)
