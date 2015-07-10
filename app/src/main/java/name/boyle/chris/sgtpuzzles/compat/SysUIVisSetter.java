dcd69b522c9db1ee1bce6adec95e090f1e1351d9
Chris Boyle <chris@boyle.name>
2015-07-10 19:09:06 +0100

    Fix crash on Android 3.x during startup.

--- app/src/main/java/name/boyle/chris/sgtpuzzles/compat/SysUIVisSetter.java
+++ app/src/main/java/name/boyle/chris/sgtpuzzles/compat/SysUIVisSetter.java
@@ -9,6 +9,7 @@ import android.view.View;
 @SuppressLint("InlinedApi")
 public class SysUIVisSetter {
 	public static void set(View v, boolean fullScreen) {
+		if (v == null) return;
 		v.setSystemUiVisibility(fullScreen
 				? View.SYSTEM_UI_FLAG_LOW_PROFILE
 				: View.SYSTEM_UI_FLAG_VISIBLE);
