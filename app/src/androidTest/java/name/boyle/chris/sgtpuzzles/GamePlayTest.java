4130fb239a620ff376ba1fa7158acaac27ad2640
Chris Boyle <chris@boyle.name>
2022-04-20 23:33:42 +0100

    Fix tests
    I can't quite get ActivityScenario to work right now, patch welcome.

--- app/src/androidTest/java/name/boyle/chris/sgtpuzzles/GamePlayTest.java
+++ app/src/androidTest/java/name/boyle/chris/sgtpuzzles/GamePlayTest.java
@@ -2,6 +2,7 @@ package name.boyle.chris.sgtpuzzles;
 
 import android.content.Intent;
 import android.net.Uri;
+
 import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.action.CoordinatesProvider;
 import androidx.test.espresso.action.GeneralClickAction;
@@ -10,6 +11,7 @@ import androidx.test.espresso.action.Press;
 import androidx.test.espresso.action.Swipe;
 import androidx.test.espresso.action.Tap;
 import androidx.test.rule.ActivityTestRule;
+
 import android.view.View;
 
 import org.hamcrest.Matcher;
@@ -20,7 +22,6 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -53,10 +54,10 @@ import static org.junit.Assert.assertNotEquals;
 @RunWith(Parameterized.class)
 public class GamePlayTest {
 
-	private static final Set<String> _usedBackends = new LinkedHashSet<>();
+	private static final Set<BackendName> _usedBackends = new LinkedHashSet<>();
 	private static final Set<Object[]> _params = new LinkedHashSet<>();
 
-	private final String _backend;
+	private final BackendName _backend;
 	private final String _gameID;
 	private final ViewAction[] _viewActions;
 
@@ -65,42 +66,42 @@ public class GamePlayTest {
 			new ActivityTestRule<>(GamePlay.class, false, false);
 
 	private static void addExamples() {
-		addExample("net", "1x2:42", new GeneralClickAction(Tap.SINGLE,
+		addExample(BackendName.NET, "1x2:42", new GeneralClickAction(Tap.SINGLE,
 				squareProportions(0, -0.25), Press.FINGER, 0, 0));
-		addExample("blackbox", "w3h3m1M1:38727296",
+		addExample(BackendName.BLACKBOX, "w3h3m1M1:38727296",
 				KEYCODE_DPAD_UP, KEYCODE_DPAD_DOWN, KEYCODE_DPAD_CENTER,
 				KEYCODE_DPAD_UP, KEYCODE_DPAD_LEFT, KEYCODE_DPAD_CENTER);
-		addExample("bridges", "3x3m2:3a2c1b",
+		addExample(BackendName.BRIDGES, "3x3m2:3a2c1b",
 				KEYCODE_DPAD_UP, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_DOWN,
 				KEYCODE_DPAD_CENTER, KEYCODE_DPAD_RIGHT,
 				KEYCODE_DPAD_CENTER, KEYCODE_DPAD_RIGHT);
-		addExample("cube", "c4x4:0C56,0", KEYCODE_DPAD_DOWN, KEYCODE_DPAD_RIGHT,
+		addExample(BackendName.CUBE, "c4x4:0C56,0", KEYCODE_DPAD_DOWN, KEYCODE_DPAD_RIGHT,
 				KEYCODE_DPAD_DOWN, KEYCODE_DPAD_DOWN, KEYCODE_DPAD_UP, KEYCODE_DPAD_RIGHT,
 				KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_LEFT, KEYCODE_DPAD_LEFT, KEYCODE_DPAD_DOWN,
 				KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_LEFT);
-		addExample("dominosa", "1:011100",
+		addExample(BackendName.DOMINOSA, "1:011100",
 				KEYCODE_DPAD_DOWN, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_DOWN, KEYCODE_DPAD_RIGHT,
 				KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_UP,
 				KEYCODE_DPAD_UP, KEYCODE_DPAD_CENTER);
-		addExample("fifteen", "2x2:1,2,0,3", KEYCODE_DPAD_LEFT);
-		addExample("filling", "2x1:02", KEYCODE_DPAD_UP, KEYCODE_2);
-		addExample("flip", "2x2:edb7,d", KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_CENTER);
-		addExample("flood", "2x2:1212,6", KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_CENTER);
-		addExample("galaxies", "3x3:co",
+		addExample(BackendName.FIFTEEN, "2x2:1,2,0,3", KEYCODE_DPAD_LEFT);
+		addExample(BackendName.FILLING, "2x1:02", KEYCODE_DPAD_UP, KEYCODE_2);
+		addExample(BackendName.FLIP, "2x2:edb7,d", KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_CENTER);
+		addExample(BackendName.FLOOD, "2x2:1212,6", KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_CENTER);
+		addExample(BackendName.GALAXIES, "3x3:co",
 				KEYCODE_DPAD_DOWN, KEYCODE_DPAD_CENTER,
 				KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_CENTER,
 				KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_CENTER);
-		addExample("guess", "c2p2g2Bm:c2ab",
+		addExample(BackendName.GUESS, "c2p2g2Bm:c2ab",
 				KEYCODE_DPAD_DOWN, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_CENTER,
 				KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_CENTER);
-		addExample("inertia", "3x3:wbbSbbgms", KEYCODE_DPAD_DOWN);
-		addExample("keen", "3:_baa_3a,m6s1m3a3",
+		addExample(BackendName.INERTIA, "3x3:wbbSbbgms", KEYCODE_DPAD_DOWN);
+		addExample(BackendName.KEEN, "3:_baa_3a,m6s1m3a3",
 				KEYCODE_DPAD_UP, KEYCODE_1, KEYCODE_DPAD_RIGHT, KEYCODE_3, KEYCODE_DPAD_RIGHT,
 				KEYCODE_2, KEYCODE_DPAD_DOWN, KEYCODE_1, KEYCODE_DPAD_LEFT, KEYCODE_2,
 				KEYCODE_DPAD_LEFT, KEYCODE_3, KEYCODE_DPAD_DOWN, KEYCODE_2, KEYCODE_DPAD_RIGHT,
 				KEYCODE_1, KEYCODE_DPAD_RIGHT, KEYCODE_3);
-		addExample("lightup", "2x2:a0b", KEYCODE_DPAD_DOWN, KEYCODE_DPAD_CENTER);
-		addExample("loopy", "3x3t0:02a2a1c",
+		addExample(BackendName.LIGHTUP, "2x2:a0b", KEYCODE_DPAD_DOWN, KEYCODE_DPAD_CENTER);
+		addExample(BackendName.LOOPY, "3x3t0:02a2a1c",
 				KEYCODE_DPAD_CENTER, KEYCODE_DPAD_LEFT, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_LEFT,
 				KEYCODE_DPAD_DOWN, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_DOWN, KEYCODE_DPAD_CENTER,
 				KEYCODE_DPAD_LEFT, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_RIGHT,
@@ -110,20 +111,27 @@ public class GamePlayTest {
 				KEYCODE_DPAD_CENTER, KEYCODE_DPAD_UP, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_LEFT,
 				KEYCODE_DPAD_CENTER, KEYCODE_DPAD_LEFT, KEYCODE_DPAD_DOWN, KEYCODE_DPAD_DOWN,
 				KEYCODE_DPAD_CENTER);
-		addExample("magnets", "3x2:111,21,111,12,TTTBBB",
+		addExample(BackendName.MAGNETS, "3x2:111,21,111,12,TTTBBB",
 				KEYCODE_DPAD_UP, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_CENTER,
 				KEYCODE_DPAD_CENTER, KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_CENTER);
-		addExample("map", "3x2n5:afa,3120a", KEYCODE_DPAD_UP,
+		addExample(BackendName.MAP, "3x2n5:afa,3120a", KEYCODE_DPAD_UP,
 				KEYCODE_DPAD_CENTER, KEYCODE_DPAD_DOWN, KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_CENTER);
-		addExample("mines", "4x3:2,0,m5d9",
+		addExample(BackendName.MINES, "4x3:2,0,m5d9",
 				KEYCODE_DPAD_DOWN, KEYCODE_DPAD_DOWN, KEYCODE_DPAD_CENTER);
-		addExample("net", "1x2:12", KEYCODE_DPAD_UP, KEYCODE_D);
-		addExample("netslide", "2x2:ch116", KEYCODE_DPAD_DOWN, KEYCODE_DPAD_CENTER);
-		addExample("palisade", "2x3n3:d33", KEYCODE_L, KEYCODE_DPAD_DOWN, KEYCODE_L,
-				KEYCODE_DPAD_DOWN, KEYCODE_L);
-		addExample("pattern", "1x2:2/1/1",
+		addExample(BackendName.MOSAIC, "3x3:a4a4a6c", KEYCODE_DPAD_UP,
+				KEYCODE_DPAD_CENTER, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_RIGHT,
+				KEYCODE_DPAD_CENTER, KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_DOWN,
+				KEYCODE_DPAD_CENTER, KEYCODE_DPAD_LEFT, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_LEFT,
+				KEYCODE_DPAD_CENTER, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_DOWN, KEYCODE_DPAD_CENTER,
+				KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_CENTER);
+		addExample(BackendName.NET, "1x2:12", KEYCODE_DPAD_UP, KEYCODE_D);
+		addExample(BackendName.NETSLIDE, "2x2:ch116", KEYCODE_DPAD_DOWN, KEYCODE_DPAD_CENTER);
+		addExample(BackendName.PALISADE, "2x3n3:d33", KEYCODE_L, KEYCODE_L,
+				KEYCODE_DPAD_DOWN, KEYCODE_L, KEYCODE_L,
+				KEYCODE_DPAD_DOWN, KEYCODE_L, KEYCODE_L);
+		addExample(BackendName.PATTERN, "1x2:2/1/1", KEYCODE_DPAD_UP,
 				KEYCODE_DPAD_CENTER, KEYCODE_DPAD_DOWN, KEYCODE_DPAD_CENTER);
-		addExample("pearl", "5x5:dBaWaBgWaBeB", KEYCODE_DPAD_CENTER,
+		addExample(BackendName.PEARL, "5x5:dBaWaBgWaBeB", KEYCODE_DPAD_CENTER,
 				KEYCODE_DPAD_CENTER, KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_RIGHT,
 				KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_DOWN, KEYCODE_DPAD_DOWN, KEYCODE_DPAD_DOWN,
 				KEYCODE_DPAD_DOWN, KEYCODE_DPAD_LEFT, KEYCODE_DPAD_LEFT, KEYCODE_DPAD_LEFT,
@@ -131,7 +139,7 @@ public class GamePlayTest {
 				KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_UP, KEYCODE_DPAD_UP, KEYCODE_DPAD_LEFT,
 				KEYCODE_DPAD_LEFT, KEYCODE_DPAD_LEFT, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_CENTER,
 				KEYCODE_DPAD_UP, KEYCODE_DPAD_CENTER);
-		addExample("pegs", "4x4:PHPPHPPOPOPOPOPO", KEYCODE_DPAD_RIGHT,
+		addExample(BackendName.PEGS, "4x4:PHPPHPPOPOPOPOPO", KEYCODE_DPAD_RIGHT,
 				KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_LEFT,
 				KEYCODE_DPAD_LEFT, KEYCODE_DPAD_DOWN, KEYCODE_DPAD_DOWN, KEYCODE_DPAD_DOWN,
 				KEYCODE_DPAD_CENTER, KEYCODE_DPAD_UP, KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_RIGHT,
@@ -141,35 +149,35 @@ public class GamePlayTest {
 				KEYCODE_DPAD_CENTER, KEYCODE_DPAD_UP, KEYCODE_DPAD_UP, KEYCODE_DPAD_LEFT,
 				KEYCODE_DPAD_LEFT, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_CENTER,
 				KEYCODE_DPAD_DOWN);
-		addExample("range", "3x2:b2_4b",
+		addExample(BackendName.RANGE, "3x2:b2_4b",
 				KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_CENTER);
-		addExample("rect", "2x2:2a2a", KEYCODE_DPAD_UP, KEYCODE_DPAD_CENTER,
+		addExample(BackendName.RECT, "2x2:2a2a", KEYCODE_DPAD_UP, KEYCODE_DPAD_CENTER,
 				KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_CENTER);
-		addExample("samegame", "2x2c3s2:1,1,3,3", KEYCODE_DPAD_CENTER,
+		addExample(BackendName.SAMEGAME, "2x2c3s2:1,1,3,3", KEYCODE_DPAD_CENTER,
 				KEYCODE_DPAD_CENTER, KEYCODE_DPAD_DOWN, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_CENTER);
-		addExample("signpost", "3x2:1ccfcg6a",
+		addExample(BackendName.SIGNPOST, "3x2:1ccfcg6a",
 				KEYCODE_DPAD_CENTER, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_CENTER,
 				KEYCODE_DPAD_CENTER, KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_CENTER,
 				KEYCODE_DPAD_DOWN, KEYCODE_DPAD_LEFT, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_CENTER,
 				KEYCODE_DPAD_LEFT, KEYCODE_DPAD_CENTER);
-		addExample("singles", "2x2:1121", KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_CENTER);
-		addExample("sixteen", "2x2:1,4,3,2", KEYCODE_DPAD_UP, KEYCODE_DPAD_UP, KEYCODE_DPAD_RIGHT,
+		addExample(BackendName.SINGLES, "2x2:1121", KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_CENTER);
+		addExample(BackendName.SIXTEEN, "2x2:1,4,3,2", KEYCODE_DPAD_UP, KEYCODE_DPAD_UP, KEYCODE_DPAD_RIGHT,
 				KEYCODE_DPAD_CENTER);
-		addExample("slant", "2x2:1c1d", KEYCODE_DPAD_CENTER, KEYCODE_DPAD_RIGHT,
+		addExample(BackendName.SLANT, "2x2:1c1d", KEYCODE_DPAD_UP, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_RIGHT,
 				KEYCODE_DPAD_CENTER, KEYCODE_DPAD_DOWN, KEYCODE_DPAD_CENTER,
 				KEYCODE_DPAD_CENTER, KEYCODE_DPAD_LEFT, KEYCODE_DPAD_CENTER);
-		addExample("solo", "2j:1c,b__", KEYCODE_DPAD_RIGHT, KEYCODE_2,
+		addExample(BackendName.SOLO, "2j:1c,b__", KEYCODE_DPAD_RIGHT, KEYCODE_2,
 				KEYCODE_DPAD_DOWN, KEYCODE_1, KEYCODE_DPAD_LEFT, KEYCODE_2);
-		addExample("tents", "4x4:baj_,1,1,0,1,1,0,2,0", KEYCODE_DPAD_RIGHT,
+		addExample(BackendName.TENTS, "4x4:baj_,1,1,0,1,1,0,2,0", KEYCODE_DPAD_RIGHT,
 				KEYCODE_DPAD_CENTER, KEYCODE_DPAD_DOWN, KEYCODE_DPAD_DOWN, KEYCODE_DPAD_LEFT,
 				KEYCODE_DPAD_CENTER, KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_RIGHT,
 				KEYCODE_DPAD_CENTER);
-		addExample("towers", "3:1/2/3/2/2/1/1/2/2/3/2/1",
+		addExample(BackendName.TOWERS, "3:1/2/3/2/2/1/1/2/2/3/2/1",
 				KEYCODE_DPAD_UP, KEYCODE_3, KEYCODE_DPAD_RIGHT, KEYCODE_2, KEYCODE_DPAD_RIGHT,
 				KEYCODE_1, KEYCODE_DPAD_DOWN, KEYCODE_2, KEYCODE_DPAD_LEFT, KEYCODE_3,
 				KEYCODE_DPAD_LEFT, KEYCODE_1, KEYCODE_DPAD_DOWN, KEYCODE_2, KEYCODE_DPAD_RIGHT,
 				KEYCODE_1, KEYCODE_DPAD_RIGHT, KEYCODE_3);
-		addExample("tracks", "4x4:Cm9a,3,2,S3,4,S3,4,3,2",
+		addExample(BackendName.TRACKS, "4x4:Cm9a,3,2,S3,4,S3,4,3,2", KEYCODE_DPAD_DOWN,
 				KEYCODE_DPAD_DOWN, KEYCODE_DPAD_DOWN, KEYCODE_DPAD_DOWN, KEYCODE_DPAD_CENTER,
 				KEYCODE_DPAD_DOWN, KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_RIGHT,
 				KEYCODE_DPAD_UP, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_UP, KEYCODE_DPAD_RIGHT,
@@ -177,18 +185,18 @@ public class GamePlayTest {
 				KEYCODE_DPAD_UP, KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_RIGHT,
 				KEYCODE_DPAD_DOWN, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_DOWN, KEYCODE_DPAD_DOWN,
 				KEYCODE_DPAD_CENTER, KEYCODE_DPAD_DOWN, KEYCODE_DPAD_DOWN, KEYCODE_DPAD_CENTER);
-		addExample("twiddle", "2x2n2:4,3,2,1",
+		addExample(BackendName.TWIDDLE, "2x2n2:4,3,2,1",
 				KEYCODE_DPAD_CENTER, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_CENTER);
-		addExample("undead", "3x3:1,2,2,dLaRLL,2,0,2,2,2,0,1,0,0,2,1,2",
+		addExample(BackendName.UNDEAD, "3x3:1,2,2,dLaRLL,2,0,2,2,2,0,1,0,0,2,1,2",
 				KEYCODE_DPAD_DOWN, KEYCODE_DPAD_DOWN, KEYCODE_Z, KEYCODE_DPAD_UP, KEYCODE_Z,
 				KEYCODE_DPAD_RIGHT, KEYCODE_G, KEYCODE_DPAD_RIGHT, KEYCODE_V, KEYCODE_DPAD_DOWN,
 				KEYCODE_V);
-		addExample("unequal", "3:0D,0,0,0,0,0,0R,0,0U,",
+		addExample(BackendName.UNEQUAL, "3:0D,0,0,0,0,0,0R,0,0U,",
 				KEYCODE_DPAD_UP, KEYCODE_3, KEYCODE_DPAD_RIGHT, KEYCODE_2, KEYCODE_DPAD_RIGHT,
 				KEYCODE_1, KEYCODE_DPAD_DOWN, KEYCODE_2, KEYCODE_DPAD_LEFT, KEYCODE_3,
 				KEYCODE_DPAD_LEFT, KEYCODE_1, KEYCODE_DPAD_DOWN, KEYCODE_2, KEYCODE_DPAD_RIGHT,
 				KEYCODE_1, KEYCODE_DPAD_RIGHT, KEYCODE_3);
-		addExample("unruly", "6x6:BCCAHgBCga", KEYCODE_DPAD_UP,
+		addExample(BackendName.UNRULY, "6x6:BCCAHgBCga", KEYCODE_DPAD_UP,
 				KEYCODE_SPACE, KEYCODE_DPAD_DOWN, KEYCODE_SPACE, KEYCODE_DPAD_DOWN,
 				KEYCODE_DPAD_CENTER, KEYCODE_DPAD_DOWN, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_DOWN,
 				KEYCODE_SPACE, KEYCODE_DPAD_DOWN, KEYCODE_DPAD_CENTER, KEYCODE_DPAD_RIGHT,
@@ -204,11 +212,11 @@ public class GamePlayTest {
 				KEYCODE_SPACE, KEYCODE_DPAD_RIGHT, KEYCODE_DPAD_UP, KEYCODE_DPAD_CENTER,
 				KEYCODE_DPAD_UP, KEYCODE_DPAD_UP, KEYCODE_SPACE, KEYCODE_DPAD_UP, KEYCODE_DPAD_CENTER,
 				KEYCODE_DPAD_UP, KEYCODE_DPAD_CENTER);
-		addExample("untangle", "4:0-1,0-2,0-3,1-2,1-3,2-3", new GeneralSwipeAction(Swipe.FAST,
+		addExample(BackendName.UNTANGLE, "4:0-1,0-2,0-3,1-2,1-3,2-3", new GeneralSwipeAction(Swipe.FAST,
 				squareProportions(0, -0.42), squareProportions(0, 0.25), Press.FINGER));
 	}
 
-	private static void addExample(final String backend, final String gameID, final int... keystrokes) {
+	private static void addExample(final BackendName backend, final String gameID, final int... keystrokes) {
 		final ArrayList<ViewAction> actions = new ArrayList<>();
 		for (int key : keystrokes) {
 			actions.add(pressKey(key));
@@ -216,26 +224,23 @@ public class GamePlayTest {
 		addExample(backend, gameID, actions.toArray(new ViewAction[0]));
 	}
 
-	private static void addExample(final String backend, final String gameID, final ViewAction... actions) {
+	private static void addExample(final BackendName backend, final String gameID, final ViewAction... actions) {
 		_usedBackends.add(backend);
 		_params.add(new Object[]{backend, gameID, actions});
 	}
 
 	@Parameters(name = "{0}:{1}")
 	public static Iterable<Object[]> data() {
-		Set<String> unusedBackends = new LinkedHashSet<>(Arrays.asList(
-				getApplicationContext().getResources().getStringArray(R.array.games)));
 		addExamples();
-		for (String _usedBackend : _usedBackends) {
-			unusedBackends.remove(_usedBackend);
-		}
-		for (String unused : unusedBackends) {
-			addExample(unused, "", KEYCODE_DPAD_CENTER);  // testGameCompletion will fail appropriately
+		for (final BackendName backend : BackendName.values()) {
+			if (!_usedBackends.contains(backend)) {
+				addExample(backend, "", KEYCODE_DPAD_CENTER);  // testGameCompletion will fail appropriately
+			}
 		}
 		return _params;
 	}
 
-	public GamePlayTest(final String backend, final String gameID, final ViewAction... viewActions) {
+	public GamePlayTest(final BackendName backend, final String gameID, final ViewAction... viewActions) {
 		_backend = backend;
 		_gameID = gameID;
 		_viewActions = viewActions;
