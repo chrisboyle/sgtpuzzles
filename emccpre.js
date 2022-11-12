chrisboyle/sgtpuzzles

commit a752e7372021c1eea5df433f08e53fae1983e8ec

Try to give a more friendly message if anything goes wrong during puzzle startup. The puzzle web pages now enclose the whole puzzle (buttons, canvas, permalinks) in a div set to display:none, and instead display an apologetic message saying 'sorry, it didn't work'; then, if we get through the whole init function without crashing, we show the puzzle and hide the apology.
[originally from svn r9802]

emccpre.js
@@ -257,9 +257,16 @@ function initPuzzle() {
     // Default to giving keyboard focus to the puzzle.
     onscreen_canvas.focus();
 
-    // And run the C setup function, passing argv[1] as the fragment
+    // Run the C setup function, passing argv[1] as the fragment
     // identifier (so that permalinks of the form puzzle.html#game-id
     // can launch the specified id).
     Module.arguments = [location.hash];
     Module.run();
+
+    // And if we get here with everything having gone smoothly, i.e.
+    // we haven't crashed for one reason or another during setup, then
+    // it's probably safe to hide the 'sorry, no puzzle here' div and
+    // show the div containing the actual puzzle.
+    document.getElementById("apology").style.display = "none";
+    document.getElementById("puzzle").style.display = "inline";
 }

html/jspage.pl
@@ -71,6 +71,7 @@ ${unfinishedheading}
 ${unfinishedpara}
 
 <hr>
+<div id="puzzle" style="display: none">
 <p align=center>
   <input type="button" id="new" value="New game">
   <input type="button" id="restart" value="Restart game">
@@ -98,6 +99,13 @@ ${unfinishedpara}
   <a id="permalink-desc">by game ID</a>
   <a id="permalink-seed">by random seed</a>
 </p>
+</div>
+<div id="apology">
+Sorry, this Javascript puzzle doesn't seem to work on your web
+browser. Perhaps you have Javascript disabled, or perhaps your browser
+doesn't provide a feature they depend on. These puzzles have been
+successfully run in Firefox 19 and Chrome 25.
+</div>
 <hr>
 
 ${instructions}

commit 5a90dd9312ed2568d1d7f4b2f728614c29ee0f85

js: Give keyboard focus to the puzzle canvas at startup again
I think this has been broken since a752e73, when the canvas changed to
being hidden, and hence unable to receive keyboard focus, when the page
loaded.  I've now moved the focus() call to after the canvas gets
displayed.

emccpre.js
@@ -448,9 +448,6 @@ function initPuzzle() {
     permalink_desc = document.getElementById("permalink-desc");
     permalink_seed = document.getElementById("permalink-seed");
 
-    // Default to giving keyboard focus to the puzzle.
-    onscreen_canvas.focus();
-
     // Create the resize handle.
     var resize_handle = document.createElement("canvas");
     resize_handle.width = 10;
@@ -575,5 +572,8 @@ function initPuzzle() {
         var apology = document.getElementById("apology");
         if (apology !== null) apology.style.display = "none";
         document.getElementById("puzzle").style.display = "";
+
+        // Default to giving keyboard focus to the puzzle.
+        onscreen_canvas.focus();
     };
 }