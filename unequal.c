4bd8822725794989a400768cb69f600e0a5d9734
Ben Harris <bjh21@bjh21.me.uk>
2023-08-12 13:52:09 +0100

    Unequal: use new move_cursor() features for normal movement
    The shift+arrow controls for dimming clues don't currently use it
    because they're a bit fiddly.

--- unequal.c
+++ unequal.c
@@ -1617,11 +1617,9 @@ static char *interpret_move(const game_state *state, game_ui *ui,
 
 	    return dupstr(buf);
 	} else {
-            move_cursor(button, &ui->hx, &ui->hy, ds->order, ds->order, false,
-                        NULL);
-	    ui->hshow = true;
             ui->hcursor = true;
-	    return MOVE_UI_UPDATE;
+            return move_cursor(button, &ui->hx, &ui->hy, ds->order, ds->order,
+                               false, &ui->hshow);
 	}
     }
     if (ui->hshow && IS_CURSOR_SELECT(button)) {
