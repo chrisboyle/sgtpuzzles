b0c0f98448cac94baadd635188fb181f0fa7354f
Chris Boyle <chris@boyle.name>
2023-08-28 07:30:31 +0100

    A couple more Kotlin conversions (convert)

--- app/src/main/kotlin/name/boyle/chris/sgtpuzzles/backend/UsedByJNI.kt
+++ app/src/main/kotlin/name/boyle/chris/sgtpuzzles/backend/UsedByJNI.kt
@@ -1,18 +1,15 @@
-package name.boyle.chris.sgtpuzzles.backend;
+package name.boyle.chris.sgtpuzzles.backend
 
-import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
-import static java.lang.annotation.ElementType.CONSTRUCTOR;
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.PACKAGE;
-import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.CLASS;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-
-/** Like @Keep but describes why (JNI code uses the annotated thing). */
-@Retention(CLASS)
-@Target({PACKAGE, TYPE, ANNOTATION_TYPE, CONSTRUCTOR, METHOD, FIELD})
-public @interface UsedByJNI {
-}
+/** Like @Keep but describes why (JNI code uses the annotated thing).  */
+@Retention(AnnotationRetention.BINARY)
+@Target(
+    AnnotationTarget.FILE,
+    AnnotationTarget.CLASS,
+    AnnotationTarget.ANNOTATION_CLASS,
+    AnnotationTarget.CONSTRUCTOR,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER,
+    AnnotationTarget.FIELD
+)
+annotation class UsedByJNI 
--- app/src/main/kotlin/name/boyle/chris/sgtpuzzles/config/PrefsConstants.kt
+++ app/src/main/kotlin/name/boyle/chris/sgtpuzzles/config/PrefsConstants.kt
@@ -1,44 +1,42 @@
-package name.boyle.chris.sgtpuzzles.config;
+package name.boyle.chris.sgtpuzzles.config
 
-/** Constants used for preference names/prefixes/values. */
-abstract class PrefsConstants {
-    private PrefsConstants() {}
-
-    static final String CATEGORY_CHOOSER = "gameChooser";
-    static final String CHOOSER_STYLE_KEY = "chooserStyle";
-    static final String CATEGORY_THIS_GAME = "thisGame";
-    static final String PLACEHOLDER_NO_ARROWS = "arrowKeysUnavailable";
-    static final String PLACEHOLDER_SEND_FEEDBACK = "send_feedback";
-    static final String SAVED_BACKEND = "savedBackend";
-    static final String LAST_PARAMS_PREFIX = "last_params_";
-    static final String STATE_PREFS_NAME = "state";
-    static final String ORIENTATION_KEY = "orientation";
-    static final String ARROW_KEYS_KEY_SUFFIX = "ArrowKeys";
-    static final String LIMIT_DPI_KEY = "limitDpi";
-    static final String KEYBOARD_BORDERS_KEY = "keyboardBorders";
-    static final String BRIDGES_SHOW_H_KEY = "bridgesShowH";
-    static final String UNEQUAL_SHOW_H_KEY = "unequalShowH";
-    static final String LATIN_SHOW_M_KEY = "latinShowM";
-    static final String FULLSCREEN_KEY = "fullscreen";
-    static final String STAY_AWAKE_KEY = "stayAwake";
-    static final String UNDO_REDO_KBD_KEY = "undoRedoOnKeyboard";
-    static final boolean UNDO_REDO_KBD_DEFAULT = true;
-    static final String MOUSE_LONG_PRESS_KEY = "extMouseLongPress";
-    static final String MOUSE_BACK_KEY = "extMouseBackKey";
-    static final String PATTERN_SHOW_LENGTHS_KEY = "patternShowLengths";
-    static final String COMPLETED_PROMPT_KEY = "completedPrompt";
-    static final String VICTORY_FLASH_KEY = "victoryFlash";
-    static final String CONTROLS_REMINDERS_KEY = "controlsReminders";
-    static final String OLD_PUZZLESGEN_LAST_UPDATE = "puzzlesgen_last_update";
-    static final String SAVED_COMPLETED_PREFIX = "savedCompleted_";
-    static final String SAVED_GAME_PREFIX = "savedGame_";
-    static final String SWAP_L_R_PREFIX = "swap_l_r_";
-    static final String UNDO_NEW_GAME_SEEN = "undoNewGameSeen";
-    static final String REDO_NEW_GAME_SEEN = "redoNewGameSeen";
-    static final String PUZZLESGEN_CLEANUP_DONE = "puzzlesgen_cleanup_done";
-    static final String AUTO_ORIENT = "autoOrient";
-    static final boolean AUTO_ORIENT_DEFAULT = true;
-    static final String NIGHT_MODE_KEY = "nightMode";
-    static final String SEEN_NIGHT_MODE = "seenNightMode";
-    static final String SEEN_NIGHT_MODE_SETTING = "seenNightModeSetting";
+/** Constants used for preference names/prefixes/values.  */
+internal object PrefsConstants {
+    const val CATEGORY_CHOOSER = "gameChooser"
+    const val CHOOSER_STYLE_KEY = "chooserStyle"
+    const val CATEGORY_THIS_GAME = "thisGame"
+    const val PLACEHOLDER_NO_ARROWS = "arrowKeysUnavailable"
+    const val PLACEHOLDER_SEND_FEEDBACK = "send_feedback"
+    const val SAVED_BACKEND = "savedBackend"
+    const val LAST_PARAMS_PREFIX = "last_params_"
+    const val STATE_PREFS_NAME = "state"
+    const val ORIENTATION_KEY = "orientation"
+    const val ARROW_KEYS_KEY_SUFFIX = "ArrowKeys"
+    const val LIMIT_DPI_KEY = "limitDpi"
+    const val KEYBOARD_BORDERS_KEY = "keyboardBorders"
+    const val BRIDGES_SHOW_H_KEY = "bridgesShowH"
+    const val UNEQUAL_SHOW_H_KEY = "unequalShowH"
+    const val LATIN_SHOW_M_KEY = "latinShowM"
+    const val FULLSCREEN_KEY = "fullscreen"
+    const val STAY_AWAKE_KEY = "stayAwake"
+    const val UNDO_REDO_KBD_KEY = "undoRedoOnKeyboard"
+    const val UNDO_REDO_KBD_DEFAULT = true
+    const val MOUSE_LONG_PRESS_KEY = "extMouseLongPress"
+    const val MOUSE_BACK_KEY = "extMouseBackKey"
+    const val PATTERN_SHOW_LENGTHS_KEY = "patternShowLengths"
+    const val COMPLETED_PROMPT_KEY = "completedPrompt"
+    const val VICTORY_FLASH_KEY = "victoryFlash"
+    const val CONTROLS_REMINDERS_KEY = "controlsReminders"
+    const val OLD_PUZZLESGEN_LAST_UPDATE = "puzzlesgen_last_update"
+    const val SAVED_COMPLETED_PREFIX = "savedCompleted_"
+    const val SAVED_GAME_PREFIX = "savedGame_"
+    const val SWAP_L_R_PREFIX = "swap_l_r_"
+    const val UNDO_NEW_GAME_SEEN = "undoNewGameSeen"
+    const val REDO_NEW_GAME_SEEN = "redoNewGameSeen"
+    const val PUZZLESGEN_CLEANUP_DONE = "puzzlesgen_cleanup_done"
+    const val AUTO_ORIENT = "autoOrient"
+    const val AUTO_ORIENT_DEFAULT = true
+    const val NIGHT_MODE_KEY = "nightMode"
+    const val SEEN_NIGHT_MODE = "seenNightMode"
+    const val SEEN_NIGHT_MODE_SETTING = "seenNightModeSetting"
 }
