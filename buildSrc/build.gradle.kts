d36ce8d0c3c72c586251f83198daa375cd2fd4d4
Chris Boyle <chris@boyle.name>
2023-10-04 08:34:49 +0100

    Bump dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,5 +9,5 @@ repositories {
 
 dependencies {
     implementation(kotlin("script-runtime"))
-    implementation("com.android.tools.build:gradle-api:8.1.1")
+    implementation("com.android.tools.build:gradle-api:8.1.2")
 }
