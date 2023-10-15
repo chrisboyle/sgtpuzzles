1be038a19b9b86f47a079ead07c2765188c841b0
Chris Boyle <chris@boyle.name>
2023-10-15 13:06:53 +0100

    Bump version: let's do a beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
         applicationId = "name.boyle.chris.sgtpuzzles"
         minSdk = 21
         targetSdk = 34
-        versionCode = 138
+        versionCode = 139
         versionName = timestamp(false)
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
