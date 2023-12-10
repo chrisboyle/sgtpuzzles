7ddb34211f7a073403587d91b223033a689b2095
Chris Boyle <chris@boyle.name>
2023-12-10 09:41:42 +0000

    Bump AndroidX dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -114,11 +114,11 @@ dependencies {
     implementation("androidx.gridlayout:gridlayout:1.0.0")
     implementation("androidx.annotation:annotation:1.7.0")
     implementation("androidx.preference:preference-ktx:1.2.1")
-    implementation("androidx.webkit:webkit:1.8.0")
+    implementation("androidx.webkit:webkit:1.9.0")
 
     val composeBom = platform("androidx.compose:compose-bom:2023.09.02")
     implementation(composeBom)
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.activity:activity-compose:1.8.1")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
