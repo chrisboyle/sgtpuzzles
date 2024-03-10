53763fa67bac6e78b0863372997851e8911c6d4c
Chris Boyle <chris@boyle.name>
2024-03-10 23:11:13 +0000

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
         applicationId = "name.boyle.chris.sgtpuzzles"
         minSdk = 21
         targetSdk = 34
-        versionCode = 143
+        versionCode = 144
         versionName = timestamp(false)
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
