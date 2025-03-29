784b16afb99e14ed506d083ed750c65a0ed8d9bc
Chris Boyle <chris@boyle.name>
2025-03-29 13:29:20 +0000

    Bump AGP & dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ fun issuesURL(): String {
 
 android {
     namespace = "name.boyle.chris.sgtpuzzles"
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "name.boyle.chris.sgtpuzzles"
         minSdk = 21
@@ -108,17 +108,17 @@ androidComponents {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.22")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:2.0.20")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.gridlayout:gridlayout:1.0.0")
-    implementation("androidx.annotation:annotation:1.8.0")
+    implementation("androidx.annotation:annotation:1.9.1")
     implementation("androidx.preference:preference-ktx:1.2.1")
-    implementation("androidx.webkit:webkit:1.11.0")
+    implementation("androidx.webkit:webkit:1.13.0")
 
     val composeBom = platform("androidx.compose:compose-bom:2024.05.00")
     implementation(composeBom)
-    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
@@ -127,7 +127,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.mockito:mockito-core:5.5.0")
     androidTestImplementation(composeBom)
-    androidTestImplementation("androidx.annotation:annotation:1.8.0")
+    androidTestImplementation("androidx.annotation:annotation:1.9.1")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test:rules:1.6.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
